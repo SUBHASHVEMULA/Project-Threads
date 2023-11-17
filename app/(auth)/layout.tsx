9933208361348d0b9771da6dbe64adb633db0d23
DevSUB-007 <subhashvemula03@gmail.com>
2023-11-18 03:33:08 +0530

    Latest Commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 
 import '../globals.css';
+// FOR BETTER SEO??
 export const metadata={
     title:'Threads',
     description:'A Next.js 13 Meta Threads Application'
@@ -9,18 +10,14 @@ export const metadata={
 
 const inter=Inter({subsets: ["latin"]})
 
-export default function RootLayout({
-    //props
-    children 
-}:{
-    //types of props
-    children: React.ReactNode
-}){
+export default function RootLayout({children}:{children:React.ReactNode}){
     return(
         <ClerkProvider>
         <html lang="en">
-            <body className="{`${inter.className} bg-dark-1`}">
-                {children}
+            <body className={`${inter.className} bg-dark-1`}>
+                <div className="w-full flex justify-center items-center min-h-screen">
+                  {children}
+                </div>
             </body>
         </html>
         </ClerkProvider>
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,12 +1,17 @@
-import * as z from 'zod';
+import * as z from "zod";
 
-export const UserValidation=z.object({
-    profile_photo:z.string().url().nonempty(),
-    name:z.string().min(3,{message:'MINIMUM 3 CHARACTERS'}).max(30),
-    username:z.string().min(3,{
-        message:'Username must be atleast 3 characters.'
-    }).max(30),
-    bio:z.string().min(3,{
-        message:'Bio must be atleast 3 characters.'
-    }).max(1000),
-})
+export const UserValidation = z.object({
+  profile_photo: z.string().url(),
+  name: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  username: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  bio: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(1000, { message: "Maximum 1000 caracters." }),
+});
